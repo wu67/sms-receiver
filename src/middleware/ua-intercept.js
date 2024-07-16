6ff9d23cf18b4e2e1477bd9fa5597a7f5421d1fd
wu67 <wk7113@gmail.com>
2024-07-16 12:04:39 +0800

    change: ua检验兼容性处理.

--- src/middleware/ua-intercept.js
+++ src/middleware/ua-intercept.js
@@ -1,6 +1,11 @@
 module.exports = function () {
   return async (ctx, next) => {
-    if (ctx.header['user-agent'] !== require('../../config').ua) {
+    if (
+      // chrome发起的请求无法更改user-agent. 只能新定一个属性用来检验.
+      ![ctx.header['ua'], ctx.header['user-agent']].includes(
+        require('../../config').ua
+      )
+    ) {
       ctx.status = 404
     } else {
       await next()
